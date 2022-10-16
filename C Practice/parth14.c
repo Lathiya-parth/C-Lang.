401ff4083253eb2e8fafe4146abec0367c486dd8
JAY01G <104667643+JAY01G@users.noreply.github.com>
2022-10-16 11:01:00 +0530

    Update parth14.c

--- C Practice/parth14.c
+++ C Practice/parth14.c
@@ -18,4 +18,25 @@ int factoriel(int number)
   else {
     return (number * factoriel(number - 1));
   } 
+    
+//     Factorial without recursion
+    
+        int n, i;
+    unsigned long long factorial = 1;
+ 
+    printf("Enter a number to find factorial: ");
+    scanf("%d",&n);
+ 
+    if (n < 0)
+        printf("Error! Please enter any positive integer number");
+ 
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;             
+        }
+        printf("Factorial of Number %d = %llu", n, factorial);
+    }
+    
 }
